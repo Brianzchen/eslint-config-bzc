4e1cc482ea405ac5baabbf4ac86a44a3218e5a49
Brian C <bzchen93@gmail.com>
2024-03-18 10:42:49 +1100

    remove old rules

--- index.js
+++ index.js
@@ -114,10 +114,8 @@ module.exports = {
     'react-hooks/rules-of-hooks': 2,
     'testing-library/no-dom-import': 2,
     'testing-library/no-manual-cleanup': 2,
-    'testing-library/no-wait-for-empty-callback': 2,
     'testing-library/no-wait-for-side-effects': 2,
     'testing-library/prefer-screen-queries': 2,
-    'testing-library/prefer-wait-for': 2,
     'unicorn/import-style': [2, {
       styles: {
         react: {
